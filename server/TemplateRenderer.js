948bcea8a7a5f0e1f20885c66a5a1ea9a75f496a
Sennio <b.bosse1991@gmail.com>
2024-07-25 17:32:22 +0200

    Fixed formatting

--- server/TemplateRenderer.js
+++ server/TemplateRenderer.js
@@ -1,37 +1,37 @@
 module.exports = class TemplateRenderer {
-    constructor(data) {
-        this.data = data;
-    }
+  constructor(data) {
+    this.data = data;
+  }
 
-    getValue(obj, path) {
-        return path.split('.').reduce((acc, part) => acc && acc[part], obj);
-    }
+  getValue(obj, path) {
+    return path.split('.').reduce((acc, part) => acc && acc[part], obj);
+  }
 
-    renderTemplate(template) {
-        return template.replace(/\{\{(\w+(\.\w+)*|\w+\[\d+\](\.\w+)*)\}\}/g, (match, p1) => {
-        let path = p1.replace(/\[(\d+)\]/g, '.$1');
-        let value = this.getValue(this.data, path);
-        return value !== undefined ? value : match;
-        });
-    }
+  renderTemplate(template) {
+    return template.replace(/\{\{(\w+(\.\w+)*|\w+\[\d+\](\.\w+)*)\}\}/g, (match, p1) => {
+      let path = p1.replace(/\[(\d+)\]/g, '.$1');
+      let value = this.getValue(this.data, path);
+      return value !== undefined ? value : match;
+    });
+  }
 
-    renderTemplateWithArraySupport(template) {
-        // Render arrays
-        template = template.replace(/\{\{#each (\w+)\}\}([\s\S]*?)\{\{\/each\}\}/g, (match, p1, p2) => {
-        let array = this.data[p1];
-        if (Array.isArray(array)) {
-            return array.map(item => new TemplateRenderer({ ...this.data, this: item }).renderTemplate(p2)).join('');
-        }
-        return match;
-        });
+  renderTemplateWithArraySupport(template) {
+    // Render arrays
+    template = template.replace(/\{\{#each (\w+)\}\}([\s\S]*?)\{\{\/each\}\}/g, (match, p1, p2) => {
+      let array = this.data[p1];
+      if (Array.isArray(array)) {
+        return array.map(item => new TemplateRenderer({ ...this.data, this: item }).renderTemplate(p2)).join('');
+      }
+      return match;
+    });
 
-        // Render other placeholders
-        return this.renderTemplate(template);
-    }
+    // Render other placeholders
+    return this.renderTemplate(template);
+  }
 
-    render(template) {
-        return this.renderTemplateWithArraySupport(template);
-    }
+  render(template) {
+    return this.renderTemplateWithArraySupport(template);
+  }
 }
 
 // Examples:
